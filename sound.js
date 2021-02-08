75d2126cfc6f9fcf183de38c0722e01d20cc0f28
Alisa Kolot <kolot.alisa@gmail.com>
2021-02-07 19:52:56 -0600

    __init__.py

---
+++ sound.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get movement info from python 
+    let userInput = document.getElementById("submit")
+    let newBox = document.getElementById("box")
+
+    // Play Track 
+    let track = 
+
+    // Play track when movement begins
+    
+    // userInput.addEventListener("click", (event) => {
+    //     let inputStr = document.getElementById("number-list").value
+    //     for (let i = 0; i < inputStr.length; i++) {
+    //         console.log(inputStr.charAt(i));
+    //         {noteArray[Number(inputStr.charAt(i))].play()};
+    //         inputStr.onended = function() {
+    //             alert("audio done")
+    //         }
+    //     }
+    //     // inputStr.onended = function() {
+    //     // for (let i = 0; i < inputStr.length; i++) {
+    //     //     console.log(inputStr.charAt(i));
+    //     //     noteArray[Number(inputStr.charAt(i))].play();
+    //     // }
+    //     // }
+        
+
+    })
+
+})
+
